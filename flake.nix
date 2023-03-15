d7a598154aefdd73dcdd8251fc9a0b1de977b783
Ryan Slama <ryanmslama@gmail.com>
2023-03-15 18:49:48 -0400

    Initial nix and pyproject

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A starter Python project so I can get running faster.";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default =
+          let
+            python = pkgs.python311;
+          in
+          pkgs.mkShell {
+            packages = [
+              (python.withPackages (ps: with ps; [
+              ]))
+              pkgs.poetry
+            ];
+          };
+      });
+    };
+}
